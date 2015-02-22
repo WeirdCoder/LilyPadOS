d924cfcbcd25f5b3c628419f780457c2c08c9add
ifenta <ifenta@mit.edu>
2015-02-22 04:58:10 -0500

    Update listener_select.py

--- 04Dan/listener_select.py
+++ 04Dan/listener_select.py
@@ -4,3 +4,21 @@ from lilylcm import 04Dan
 
 def my_handler(channel, data):
     msg = 04Dan.decode(data)
+    print("Received message on channel /"%s/"" % channel)
+    print("  count  = %s" % str(msg.count))
+    print("  done   = %s" % str(msg.done))
+    print("  value  = %s" % str(msg.value))
+    print("  name   = %s" % str(msg.name))
+    print("  cured  = %s" % str(msg.cured))
+    print("")
+    
+lc = lcm.LCM()
+subscription = lc.subscribe("EXAMPLE", my_handler)
+
+try:
+    timeout = 1.5 # amount of time to wait, in seconds
+    while True:
+        rfds, wfds, efds = select.select([lc.fileno()], [], [], timeout)
+        if rfds:
+            lc.handle()
+            
