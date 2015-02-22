b675125b56332d013ce3f17cc0d7da4d32bf22b8
ifenta <ifenta@mit.edu>
2015-02-22 05:33:50 -0500

    Update listener.py

--- 04Dan/listener.py
+++ 04Dan/listener.py
@@ -1,14 +1,14 @@
 import lcm
-from lilylcm import 04Dan
+from lilylcm import 03Citrus
 
 def my_handler(channel, data):
-    msg = 04Dan.decode(data)
+    msg = 03Citrus.decode(data)
     print("Received message on channel /"%s/"" % channel)
     print("  value  = %s" % str(msg.value))
     print("")
     
 lc = lcm.LCM()
-subscription = lc.subscribe("EXAMPLE", my_handler)
+subscription = lc.subscribe("03Citrus", my_handler)
 
 try:
     while True:
