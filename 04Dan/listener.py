31d6eb44f02360769ce8e65c5c6bf56acfeee6b3
ifenta <ifenta@mit.edu>
2015-02-22 05:23:50 -0500

    Update listener.py

--- 04Dan/listener.py
+++ 04Dan/listener.py
@@ -4,11 +4,7 @@ from lilylcm import 04Dan
 def my_handler(channel, data):
     msg = 04Dan.decode(data)
     print("Received message on channel /"%s/"" % channel)
-    print("  count  = %s" % str(msg.count))
-    print("  done   = %s" % str(msg.done))
     print("  value  = %s" % str(msg.value))
-    print("  name   = %s" % str(msg.name))
-    print("  cured  = %s" % str(msg.cured))
     print("")
     
 lc = lcm.LCM()
