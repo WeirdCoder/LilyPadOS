f64bab7df3edf41a49a28951b451ea7f98654c4d
Alex Chen <chen.alex121@gmail.com>
2015-03-01 15:58:49 -0500

    05Ebola Updated

---
+++ 05Ebola/05Ebola.py
@@ -0,0 +1,23 @@
+import lcm
+import time
+
+from lilylcm import L05Ebola
+
+lc = lcm.LCM()
+
+msg = L05Ebola()
+msg.cured = True
+
+def my_handler(channel, data):
+   print("Received message from 04!")
+
+subscription = lc.subscribe("04DAN",my_handler)
+
+lc.publish("05EBOLA",msg.encode())
+
+try:
+    while True:
+        lc.handle()
+except Keyboardinterrupt:
+    pass
+lc.unsubscribe(subscription)
