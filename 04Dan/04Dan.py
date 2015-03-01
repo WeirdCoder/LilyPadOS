84637902f981b4570588b628fe655e031d708e92
ifenta <ifenta@mit.edu>
2015-03-01 15:17:31 -0500

    Update 04Dan.py

--- 04Dan/04Dan.py
+++ 04Dan/04Dan.py
@@ -11,15 +11,15 @@ msg.name = "Dark n Chunky"
 def my_handler(channel, data):
   print("Received message from 04")
   
-  subscription = lc.subscribe("04DAN", myhandler)
+subscription = lc.subscribe("04DAN", myhandler)
   
-  lc.publish("04DAN", msg.encode())
+lc.publish("04DAN", msg.encode())
   
-  try:
-    while True:
-        lc.handle()
-  except Keyboardinterrupt:
-    pass
+try:
+  while True:
+      lc.handle()
+except Keyboardinterrupt:
+  pass
     
-  lc.unsubscribe(subscription)
+lc.unsubscribe(subscription)
 
