9345513c5c685cec20be8c78dffdf0063134348d
Javier Alvarez <alvarezf@raspberrypi.(none)>
2015-03-01 16:59:15 -0500

    Edited 01Apple.py

---
+++ 01Apple/01Apple.py
@@ -0,0 +1,21 @@
+import lcm
+import time
+
+from lilylcm import L01Apple 
+
+lc = lcm.LCM()
+
+msg = L01Apple()
+msg.cured = True
+
+def my_handler(channel, data):
+   lc.publish("01Apple",msg.encode())
+
+subscription = lc.subscribe("05Ebola",my_handler)
+
+try:
+    while True:
+        lc.handle()
+except Keyboardinterrupt:
+    pass
+lc.unsubscribe(subscription)
