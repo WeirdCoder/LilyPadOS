b11378a4cf3245844f68718a556ed5706f1e5c13
ifenta <ifenta@mit.edu>
2015-03-06 01:21:14 -0500

    Update SensorMotorTest.py

--- 04Dan/SensorMotorTest.py
+++ 04Dan/SensorMotorTest.py
@@ -1,20 +1,23 @@
+import serial
 import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.OUT) ##servo
+##GPIO.setup(18, GPIO.OUT) ##servo
 ##GPIO.setup(22, GPIO.OUT) motor
 GPIO.setup(16, GPIO.IN) ##button
-pwm = GPIO.PWM(18, 100)
-pwm.start(5)
+##pwm = GPIO.PWM(18, 100)
+##pwm.start(5)
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
 try:
   while True:
     i = GPIO.input(16)
-    if i == 0:
+    port.readline()
+/*    if i == 0:
       pwm.ChangeDutyCycle(20.0 / 10.0 + 2.5)
     else:
-      pwm.ChangeDutyCycle(100.0 / 10.0 + 2.5)
+      pwm.ChangeDutyCycle(100.0 / 10.0 + 2.5)*/
     
 except KeyboardInterrupt:
   GPIO.cleanup()
