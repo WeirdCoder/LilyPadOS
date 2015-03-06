3cdd65f4ccecf5f63d99cc9a92e64154c15aac70
ifenta <ifenta@mit.edu>
2015-03-05 23:32:51 -0500

    Update and rename 04Dan to 04Dan/SensorMotorTest.py

---
+++ 04Dan/SensorMotorTest.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT) //servo
+GPIO.setup(22, GPIO.OUT) //motor
+GPIO.setup(16, GPIO.IN) //button
+
+try:
+  while True:
+    i = GPIO.input(16)
+    print(i)
+    delay(1)
+  except Keyboardinterupt:
+    pass
+    
+GPIO.cleanup()
