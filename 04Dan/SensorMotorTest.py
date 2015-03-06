ac81c9d977f1d7bb9ec2e4dac5ef777a0944e65a
ifenta <ifenta@mit.edu>
2015-03-06 01:04:26 -0500

    Update SensorMotorTest.py

--- 04Dan/SensorMotorTest.py
+++ 04Dan/SensorMotorTest.py
@@ -2,14 +2,19 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-##GPIO.setup(18, GPIO.OUT) servo
+GPIO.setup(18, GPIO.OUT) servo
 ##GPIO.setup(22, GPIO.OUT) motor
 GPIO.setup(16, GPIO.IN) ##button
+pwm = GPIO.PWM(18, 100)
+pwm.start(5)
 
 try:
   while True:
     i = GPIO.input(16)
-    print(i)
-    time.sleep(1)
+    if i == 0:
+      pwm.ChangeDutyCycle(20.0 / 10.0 + 2.5)
+    else:
+      pwm.ChangeDutyCycle(100.0 / 10.0 + 2.5)
+    
 except KeyboardInterrupt:
   GPIO.cleanup()
