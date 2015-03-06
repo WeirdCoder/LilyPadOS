475b584a4a54c80d8b27ec8893b6dd4efdb02d76
Alex Chen <chen.alex121@gmail.com>
2015-03-05 22:07:52 -0500

    Initiated Modules

--- lcmtype/lilylcm/L01Apple.py
+++ lcmtype/lilylcm/L01Apple.py
@@ -9,49 +9,49 @@ except ImportError:
     from io import BytesIO
 import struct
 
-class 05ebola(object):
-    __slots__ = ["cured"]
+class L01Apple(object):
+    __slots__ = ["count"]
 
     def __init__(self):
-        self.cured = False
+        self.count = 0
 
     def encode(self):
         buf = BytesIO()
-        buf.write(05ebola._get_packed_fingerprint())
+        buf.write(L01Apple._get_packed_fingerprint())
         self._encode_one(buf)
         return buf.getvalue()
 
     def _encode_one(self, buf):
-        buf.write(struct.pack(">b", self.cured))
+        buf.write(struct.pack(">b", self.count))
 
     def decode(data):
         if hasattr(data, 'read'):
             buf = data
         else:
             buf = BytesIO(data)
-        if buf.read(8) != 05ebola._get_packed_fingerprint():
+        if buf.read(8) != L01Apple._get_packed_fingerprint():
             raise ValueError("Decode error")
-        return 05ebola._decode_one(buf)
+        return L01Apple._decode_one(buf)
     decode = staticmethod(decode)
 
     def _decode_one(buf):
-        self = 05ebola()
-        self.cured = bool(struct.unpack('b', buf.read(1))[0])
+        self = L01Apple()
+        self.count = struct.unpack(">b", buf.read(1))[0]
         return self
     _decode_one = staticmethod(_decode_one)
 
     _hash = None
     def _get_hash_recursive(parents):
-        if 05ebola in parents: return 0
-        tmphash = (0xad85c9a8b574d497) & 0xffffffffffffffff
+        if L01Apple in parents: return 0
+        tmphash = (0x4ca686c4e8b59946) & 0xffffffffffffffff
         tmphash  = (((tmphash<<1)&0xffffffffffffffff)  + (tmphash>>63)) & 0xffffffffffffffff
         return tmphash
     _get_hash_recursive = staticmethod(_get_hash_recursive)
     _packed_fingerprint = None
 
     def _get_packed_fingerprint():
-        if 05ebola._packed_fingerprint is None:
-            05ebola._packed_fingerprint = struct.pack(">Q", 05ebola._get_hash_recursive([]))
-        return 05ebola._packed_fingerprint
+        if L01Apple._packed_fingerprint is None:
+            L01Apple._packed_fingerprint = struct.pack(">Q", L01Apple._get_hash_recursive([]))
+        return L01Apple._packed_fingerprint
     _get_packed_fingerprint = staticmethod(_get_packed_fingerprint)
 
--- lcmtype/lilylcm/L04Dan.py
+++ lcmtype/lilylcm/L04Dan.py
@@ -9,49 +9,53 @@ except ImportError:
     from io import BytesIO
 import struct
 
-class ebola(object):
-    __slots__ = ["cured"]
+class L04Dan(object):
+    __slots__ = ["name"]
 
     def __init__(self):
-        self.cured = False
+        self.name = ""
 
     def encode(self):
         buf = BytesIO()
-        buf.write(ebola._get_packed_fingerprint())
+        buf.write(L04Dan._get_packed_fingerprint())
         self._encode_one(buf)
         return buf.getvalue()
 
     def _encode_one(self, buf):
-        buf.write(struct.pack(">b", self.cured))
+        __name_encoded = self.name.encode('utf-8')
+        buf.write(struct.pack('>I', len(__name_encoded)+1))
+        buf.write(__name_encoded)
+        buf.write(b"\0")
 
     def decode(data):
         if hasattr(data, 'read'):
             buf = data
         else:
             buf = BytesIO(data)
-        if buf.read(8) != ebola._get_packed_fingerprint():
+        if buf.read(8) != L04Dan._get_packed_fingerprint():
             raise ValueError("Decode error")
-        return ebola._decode_one(buf)
+        return L04Dan._decode_one(buf)
     decode = staticmethod(decode)
 
     def _decode_one(buf):
-        self = ebola()
-        self.cured = bool(struct.unpack('b', buf.read(1))[0])
+        self = L04Dan()
+        __name_len = struct.unpack('>I', buf.read(4))[0]
+        self.name = buf.read(__name_len)[:-1].decode('utf-8', 'replace')
         return self
     _decode_one = staticmethod(_decode_one)
 
     _hash = None
     def _get_hash_recursive(parents):
-        if ebola in parents: return 0
-        tmphash = (0xad85c9a8b574d497) & 0xffffffffffffffff
+        if L04Dan in parents: return 0
+        tmphash = (0x90df9b84b9cebd12) & 0xffffffffffffffff
         tmphash  = (((tmphash<<1)&0xffffffffffffffff)  + (tmphash>>63)) & 0xffffffffffffffff
         return tmphash
     _get_hash_recursive = staticmethod(_get_hash_recursive)
     _packed_fingerprint = None
 
     def _get_packed_fingerprint():
-        if ebola._packed_fingerprint is None:
-            ebola._packed_fingerprint = struct.pack(">Q", ebola._get_hash_recursive([]))
-        return ebola._packed_fingerprint
+        if L04Dan._packed_fingerprint is None:
+            L04Dan._packed_fingerprint = struct.pack(">Q", L04Dan._get_hash_recursive([]))
+        return L04Dan._packed_fingerprint
     _get_packed_fingerprint = staticmethod(_get_packed_fingerprint)
 
