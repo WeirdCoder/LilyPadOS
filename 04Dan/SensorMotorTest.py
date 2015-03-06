9d35ea5d25bacc478134e21bdd615e4f4befa328
ifenta <ifenta@mit.edu>
2015-03-06 01:05:32 -0500

    Update SensorMotorTest.py

--- 04Dan/SensorMotorTest.py
+++ 04Dan/SensorMotorTest.py
@@ -2,7 +2,7 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.OUT) servo
+GPIO.setup(18, GPIO.OUT) ##servo
 ##GPIO.setup(22, GPIO.OUT) motor
 GPIO.setup(16, GPIO.IN) ##button
 pwm = GPIO.PWM(18, 100)
