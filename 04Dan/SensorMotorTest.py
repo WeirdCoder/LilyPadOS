828eef7b2c9bd2e3d342256eb73238db2f157866
Isaac Fenta <ifenta@raspberrypi.(none)>
2015-03-07 20:52:53 -0500

    motor and arduino stuff

--- 04Dan/SensorMotorTest.py
+++ 04Dan/SensorMotorTest.py
@@ -2,23 +2,28 @@ import serial
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
+##GPIO.setmode(GPIO.BOARD)
 ##GPIO.setup(18, GPIO.OUT) ##servo
 ##GPIO.setup(22, GPIO.OUT) motor
-GPIO.setup(16, GPIO.IN) ##button
+##GPIO.setup(16, GPIO.IN) ##button
+
 ##pwm = GPIO.PWM(18, 100)
 ##pwm.start(5)
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+
+port = serial.Serial("/dev/ttyACM0", baudrate=9600)
 
 try:
   while True:
-    i = GPIO.input(16)
-    port.readline()
-##    if i == 0:
-##      pwm.ChangeDutyCycle(20.0 / 10.0 + 2.5)
-##    else:
-##      pwm.ChangeDutyCycle(100.0 / 10.0 + 2.5)
-    
+    ##i = GPIO.input(16)
+    ##print port.readline()
+    port.writelines("Hi Arduino :)")
+    '''
+    if i == 0:
+      pwm.ChangeDutyCycle(20.0 / 10.0 + 2.5)
+    else:
+      pwm.ChangeDutyCycle(100.0 / 10.0 + 2.5)
+    '''
+  time.sleep(0.05)
     
 except KeyboardInterrupt:
-  GPIO.cleanup()
+  ##GPIO.cleanup()
