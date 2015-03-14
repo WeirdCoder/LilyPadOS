7962d353cb454705aff01e4447d42d47f7166c48
Alex Chen <chen.alex121@gmail.com>
2015-03-13 21:50:31 -0400

    x

--- 09I2C/09I2C.py
+++ 09I2C/09I2C.py
@@ -15,13 +15,15 @@ depthI2C = Adafruit_I2C(depthAddress)
 adcI2C   = Adafruit_I2C(adcAddress)
 tempI2C  = Adafruit_I2C(tempAddress)
 
-##How to Use i2c 
-#Writing a list of Byte to a register on device. you can also just put in a single byte
-#yourI2C.writeList(register,listOfData)
+'''How to Use i2c 
+Writing a list of Byte to a register on device. you can also just put in a single byte
+yourI2C.writeList(register,listOfData)
 
-#Reading a number of byte from a register on a device.
-#yourI2C.readList(register,numberOfBytes)
+Reading a number of byte from a register on a device.
+yourI2C.readList(register,numberOfBytes)
 
+There are also various read and write types like for just a single bit (Remember that a byte consist of 8 bits.)  And also always search for 3rd party codes.  If you can get 3rd party code that does the i2C readout for you, do it!
+'''
 
 delay = 5
 while(true)
