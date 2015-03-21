19094161472f67e7bc7e7fa53497ef33f042d73c
Emma Nelson <ejnelson@raspberrypi.(none)>
2015-03-21 19:04:40 -0400

    Transistor

--- 11Charger/11Charger.py
+++ 11Charger/11Charger.py
@@ -1,7 +1,27 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7, GPIO.OUT)
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+import time
 
+stop = time.time() + 30
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+GPIO.output(26, GPIO.LOW)
+GPIO.output(17, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
+GPIO.output(22, GPIO.LOW)
+
+GPIO.output(26, GPIO.HIGH)
+GPIO.output(17, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
+GPIO.output(22, GPIO.HIGH)
+
+while time.time() < stop:
+   print "hello world"
+GPIO.output(26, GPIO.LOW)
+GPIO.output(17, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
+GPIO.output(22, GPIO.LOW)
