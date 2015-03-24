744100006c19e781d25ab269fcf95e18fc5b3fa9
Isaac Fenta <ifenta@raspberrypi.(none)>
2015-03-24 18:19:34 -0400

    10Docking code update

--- 10Docking/10Docking.py
+++ 10Docking/10Docking.py
@@ -2,18 +2,33 @@ import serial
 import lcm
 import time
 
-from lilylcm import ##activation signal to turn on docking lights
+from lilylcm import L21DockDetect
 
 lc = lcm.LCM()
 
 port = serial.Serial("/dev/ttyACM0", baudrate = 9600) ##change to what ever port the arduino is connected to
 
-sub = lc.subscribe("ACTIVATION", my_handler) ##change to activation class name
+msg = L21DockDetect()
+
+def my_handler(channel, data):
+  if port.readline() == "ON":
+    msg.detected = True
+    lc.publish("POD_DockDetect", msg.encode())
+  else if port.readline() == "OFF":
+    msg.detected = False
+    lc.publish("POD_DockDetect", msg.encode())
+  else:
+    pass
+
+subL = lc.subscribe("POD_LED", my_handler)
+subD = lc.subscribe("POD_Magnet", my_handler)
 
 try:
   while True:
     port.write(str(sub.activate))
+    lc.handle()
     time.sleep(0.05)
 except KeyboardInterrupt:
-  lc.unsubscribe(sub)
+  lc.unsubscribe(subL)
+  lc.unsubscribe(subD)
 
