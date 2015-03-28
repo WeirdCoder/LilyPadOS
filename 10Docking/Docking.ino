a0864c7ce0b9ef7512ec614ccce6a6fe6b152b48
ifenta <ifenta@mit.edu>
2015-03-28 16:44:47 -0400

    Update Docking.ino

--- 10Docking/Docking.ino
+++ 10Docking/Docking.ino
@@ -5,15 +5,15 @@ Servo magnetServo;
 void setup(){
   Serial.begin(9600);
   pinMode(2, OUTPUT); //relays for LED
-  pinMode(3, INPUT) //dock detection circuit
+  pinMode(3, INPUT); //dock detection circuit
   magnetServo.attach(9);
   magnetServo.write(1);
   digitalWrite(2, LOW);
 }
 
 void loop(){
-  int val = None;
-  if (val == None || val != digitalRead(3)){
+  int val = 789789;
+  if (val == 789789 || val != digitalRead(3)){
     val = digitalRead(3); 
     
     if (val == HIGH){
@@ -33,36 +33,36 @@ void loop(){
   
   else{}
 
-  if (Serial.available()){
-    int i = Serial.read();
+  /*if (Serial.available()){
+    char i = Serial.read();
     
-    if (i == "00"){
+    if (i == '0'){
       digitalWrite(2, LOW); 
-      magnetServo.write(1);
+      magnetServo.write(20);
       Serial.println("000"); //healthy
     }
     
-    else if (i == "01"){
+    else if (i == '1'){
       digitalWrite(2, LOW); 
-      magnetServo.write(90);
+      magnetServo.write(30);
       Serial.println("000"); //healthy
     }
     
-    else if (i == "10"){
+    else if (i == '2'){
       digitalWrite(2, HIGH); 
-      magnetServo.write(1);
+      magnetServo.write(40);
       Serial.println("000"); //healthy
     }
     
-    else if (i == "11"){
+    else if (i == '3'){
       digitalWrite(2, HIGH); 
-      magnetServo.write(HIGH);
+      magnetServo.write(60);
       Serial.println("000"); //healthy
     }
   
     else{
       Serial.println("002"); //Something is wrong with the RPi Communication
     }  
-  }
+  }*/
   delay(50); 
 }
