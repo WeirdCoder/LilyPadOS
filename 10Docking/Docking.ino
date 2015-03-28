d6836aab33c687ee96aebb8b9d036764db9b5074
ifenta <ifenta@mit.edu>
2015-03-28 14:44:05 -0400

    Update Docking.ino

--- 10Docking/Docking.ino
+++ 10Docking/Docking.ino
@@ -5,6 +5,7 @@ Servo magnetServo;
 void setup(){
   Serial.begin(9600);
   pinMode(2, OUTPUT); //relays for LED
+  pinMode(3, INPUT) //dock detection circuit
   magnetServo.attach(9);
   magnetServo.write(1);
   digitalWrite(2, LOW);
@@ -12,20 +13,44 @@ void setup(){
 
 void loop(){
   
+  if (digitalRead(3) == HIGH){
+    Serial.write("ON");
+  }
+  
+  else if (digitalRead(3) == LOW){
+    Serial.write("OFF");
+  }
+  
+  else{
+    Serial.println("Something is wrong with the detection circuit");
+  }
+  
   if (Serial.available()){
     int i = Serial.read();
-    if (i-48){
-      digitalWrite(2, HIGH); 
-      magnetServo.write(90);
-      Serial.write("ON");
+    
+    if (i == "00"){
+      digitalWrite(2, LOW); 
+      magnetServo.write(1);
     }
     
-    else{
-      digitalWrite(2, LOW);
+    else if (i == "01"){
+      digitalWrite(2, LOW); 
+      magnetServo.write(90);
+    }
+    
+    else if (i == "10"){
+      digitalWrite(2, HIGH); 
       magnetServo.write(1);
-      Serial.write("OFF");
+    }
+    
+    else if (i == "11"){
+      digitalWrite(2, HIGH); 
+      magnetServo.write(HIGH);
     }
   
+    else{
+      Serial.println("Something is wrong with the RPi Communication");
+    }  
   }
   delay(50); 
 }
