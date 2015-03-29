e93030b960ae6f07aab4ccb9c1ddfab8d1e7b378
Alex Chen <chen.alex121@gmail.com>
2015-03-29 14:18:52 -0400

    minor changes

--- 11Charger/11Charger.py
+++ 11Charger/11Charger.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+
 stop = time.time() + 30
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -14,6 +15,22 @@ GPIO.output(17, GPIO.LOW)
 GPIO.output(27, GPIO.LOW)
 GPIO.output(22, GPIO.LOW)
 
+
+
+'''buzzer Interrupt'''
+buzzerPin = 1 #TODO define buzzer Pin
+def buzzer_callback(gpio_id, value):
+    #gpio_id that is triggered.
+    #value is 1 for on(5V) and 0 for off (GND).
+    x = value #TODO Do something
+def watch_buzzer():
+    #Start watching the pin, it will report every time the buzzer is turned on, and the next time waiting for 1 second.
+    RPIO.add_interrupt_callback(buzzerPin, buzzer_callback, edge='rising',debounce_timeout_ms = 1000)
+
+def unwatch_buzzer():
+    RPIO.del_interrupt_callback(buzzerPin)
+
+'''Continue to button code'''
 
 button = "Start"
 
