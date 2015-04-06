8c001e90fea56f318df66bbeca4b17592cbfb526
Alex Chen <alexc89@mit.edu>
2015-04-06 12:02:54 -0400

    AntennaBox Arduino to LCM conversion

--- 13Antenna/13Antenna.py
+++ 13Antenna/13Antenna.py
@@ -3,25 +3,36 @@
 
 import lcm
 import time
-
-from lilylcm import #channel
+from serial import Serial
+from lilylcm import L20CompassHeading
+from lilylcm import L11GPS
+from lilylcm import L13Wind 
 
 lc = lcm.LCM()
 
-msg = #instance
-msg.latitude = 0.0
-msg.longitude = 0.0
-
-def my_handler(channel, data):
-	#define the function here
-
-subscription = lc.subscribe("SOME-CHANNEL", my_handler)
-
 lc.publish("SOME-CHANNEL", msg.encode())
 
+arduinoComm = Serial('/dev/ttyUSB0',9600)#Serial
+
 try:
-	while True:
-		lc.handle()
+    while True:
+        #Read data from Arduino
+        separator = ','
+        readin = arduinoComm.readline()
+        (windDir,windMag,compassHeading,gpsLong, gpsLat) = readin.split(separator)
+        gpsMsg = L11GPS()
+        gpsMsg.latitude = gpsLat
+        gpsMsg.longtitude = gpslong
+        windMsg = L13Wind()
+        windMsg.direction = windDir - compassHeading
+        windMsg.magnitude = windMag
+        compassMsg = L20CompassHeading()
+        compassMsg.direction = compassHeading
+        lc.publish('POD_GPS',gpsMsg.encode())
+        lc.publish('POD_Wind',windMsg.encode())
+        lc.publish('POD_Compass',compassMsg.encode())
+        time.delay(1)
+        
 except Keyboardinterrupt:
 	pass
 lc.unsubscribe(subscription)
