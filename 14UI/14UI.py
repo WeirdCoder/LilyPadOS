ffa84497e79295ba7a6ea61c6fcf8ffa673cf8e1
ifenta <ifenta@mit.edu>
2015-04-14 15:33:59 -0400

    Update 14UI.py

--- 14UI/14UI.py
+++ 14UI/14UI.py
@@ -1,6 +1,12 @@
 import lcm
 import time
 
+from lilylcm import L06Depth
+from lilylcm import L07Humidity
+from lilylcm import L08Tempurature
+from lilylcm import L10ChargerCOmplete
+from lilylcm import L14LEDs
+from lilylcm import L16ChargerCommand
 from lilylcm import L19DockCommand
 
 lc = lcm.LCM()
