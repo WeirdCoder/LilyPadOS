5065085e3789972fd5d0dbffbd83f60505aff045
ifenta <ifenta@mit.edu>
2015-04-14 15:36:24 -0400

    Update 04Dan.py

--- 04Dan/04Dan.py
+++ 04Dan/04Dan.py
@@ -9,8 +9,8 @@ msg = L04Dan()
 msg.name = "Dark n Chunky"
 
 def my_handler(channel, data):
-  print("Received message from %s" % channel)
   lc.publish("04DAN", msg.encode())
+  return "Received message from %s" % channel
   
 subscription = lc.subscribe("05EBOLA", my_handler)
   
