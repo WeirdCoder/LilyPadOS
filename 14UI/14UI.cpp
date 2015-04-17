c9f3dc0a420cff15179995a0d4059ff90704f55f
ifenta <ifenta@mit.edu>
2015-04-17 14:49:05 -0400

    Update 14UI.cpp

--- 14UI/14UI.cpp
+++ 14UI/14UI.cpp
@@ -1,29 +1,56 @@
-/*import lcm
-import time
+#include <stdio.h>
 
-from lilylcm import L06Depth
-from lilylcm import L07Humidity
-from lilylcm import L08Tempurature
-from lilylcm import L14LEDs
-from lilylcm import L16ChargerCommand
-from lilylcm import L19DockCommand
+#include <lcm/lcm-cpp.hpp>
+#include "lilylcm/L06Depth.hpp"
+#include "lilylcm/L07Humidity.hpp"
+#include "lilylcm/L08Tempurature.hpp"
+#include "lilylcm/L14LEDs.hpp"
+#include "lilylcm/L16ChargerCommand.hpp"
+#include "lilylcm/L19DockCommand.hpp"
 
-lc = lcm.LCM()
+class Handler 
+{
+    public:
+        ~Handler() {}
 
-def my_handler(channel, data):
+        void handleMessage(const lcm::ReceiveBuffer* rbuf,
+                const std::string& chan, 
+                const exlcm::example_t* msg)
+        {
+            int i;
+            printf("Received message on channel \"%s\":\n", chan.c_str());
+            printf("  timestamp   = %lld\n", (long long)msg->timestamp);
+            printf("  position    = (%f, %f, %f)\n",
+                    msg->position[0], msg->position[1], msg->position[2]);
+            printf("  orientation = (%f, %f, %f, %f)\n",
+                    msg->orientation[0], msg->orientation[1], 
+                    msg->orientation[2], msg->orientation[3]);
+            printf("  ranges:");
+            for(i = 0; i < msg->num_ranges; i++)
+                printf(" %d", msg->ranges[i]);
+            printf("\n");
+            printf("  name        = '%s'\n", msg->name.c_str());
+            printf("  enabled     = %d\n", msg->enabled);
+        }
+};
 
-subDep = lc.subscribe("POD_Depth", my_handler)
-subHum = lc.subscribe("POD_Humidity", my_handler)
-subTemp = lc.subscribe("POD_Tempurature", my_handler)
-subSat = lc.subscribe("POD_LED", my_handler) 
-subChar = lc.subscribe("POD_Charge", my_handler)
-subMag = lc.subscribe("POD_Magnet", my_handler)
+int main(int argc, char** argv)
+{
+    lcm::LCM lcm;
 
-try:
-  while True:
-    lc.handle()
-    time.sleep(0.05)
-except KeyboardInterrupt:
-  lc.unsubscribe(sub)*/
-  
-  
+    if(!lcm.good())
+        return 1;
+
+    Handler handlerObject;
+    lcm.subscribe("POD_Depth", &Handler::handleMessage, &handlerObject);
+    lcm.subscribe("POD_Humidity", &Handler::handleMessage, &handlerObject);
+    lcm.subscribe("POD_Tempurature", &Handler::handleMessage, &handlerObject);
+    lcm.subscribe("POD_LED", &Handler::handleMessage, &handlerObject);
+    lcm.subscribe("POD_Charge", &Handler::handleMessage, &handlerObject);
+    lcm.subscribe("POD_Magnet", &Handler::handleMessage, &handlerObject);
+
+    while(0 == lcm.handle());
+
+    return 0;
+}
+
